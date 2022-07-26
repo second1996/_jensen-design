521fa9cc5aa85118baef6914cbe11b00b4a9197b
second1996 <vital10012@gmail.com>
2022-07-27 00:16:37 +0300

    Header. Section: Heroes. Modal: Discuss project

--- app/js/app.js
+++ app/js/app.js
@@ -1,7 +1,12 @@
 // Import vendor jQuery plugin example
 // import '~/app/libs/mmenu/dist/mmenu.js'
 
+import { Modal } from 'bootstrap';
 import { Fancybox, Carousel, Panzoom } from '@fancyapps/ui';
+import Swiper, { Pagination, Navigation, Thumbs, Controller } from 'swiper';
+
+Swiper.use([Pagination, Navigation, Thumbs, Controller]);
+
 
 document.addEventListener('DOMContentLoaded', () => {
 
@@ -26,27 +31,27 @@ document.addEventListener('DOMContentLoaded', () => {
 	/**
 	 * Mobile menu
 	 */
-	const mmenu = document.querySelector('.mmenu');
-	const burgerBtn = document.querySelector('.header-burger');
-	const mmenuCloseBtn = mmenu?.querySelector('.mmenu-heading .btn-close');
+	// const mmenu = document.querySelector('.mmenu');
+	// const burgerBtn = document.querySelector('.header-burger');
+	// const mmenuCloseBtn = mmenu?.querySelector('.mmenu-heading .btn-close');
 
-	burgerBtn.addEventListener('click', () => {
-		showMobileMenu();
-	});
+	// burgerBtn.addEventListener('click', () => {
+	// 	showMobileMenu();
+	// });
 
-	mmenuCloseBtn.addEventListener('click', () => {
-		hideMobileMenu();
-	});
+	// mmenuCloseBtn.addEventListener('click', () => {
+	// 	hideMobileMenu();
+	// });
 
-	function showMobileMenu() {
-		mmenu.classList.add('_is-active');
-		toggleBackdrop('show');
-	}
+	// function showMobileMenu() {
+	// 	mmenu.classList.add('_is-active');
+	// 	toggleBackdrop('show');
+	// }
 
-	function hideMobileMenu() {
-		mmenu.classList.remove('_is-active');
-		toggleBackdrop('hide');
-	}
+	// function hideMobileMenu() {
+	// 	mmenu.classList.remove('_is-active');
+	// 	toggleBackdrop('hide');
+	// }
 
 	/**
 	 * Smooth scroll
@@ -91,5 +96,72 @@ document.addEventListener('DOMContentLoaded', () => {
 			packageTitle.value = packageName;
 		});
 	}
+
+	/**
+	 * Rooms slider
+	 */
+	const stylingRoomsSecondarySwiper = new Swiper('.s-rooms-slider-secondary', {
+		loop: true,
+		allowTouchMove: false,
+		autoHeight: true,
+		centeredSlides: true,
+		slideToClickedSlide: false,
+		slidesPerView: 3,
+		spaceBetween: 20,
+		speed: 600,
+		navigation: {
+			prevEl: '.s-rooms-slider-secondary-navigation .swiper-button-prev',
+			nextEl: '.s-rooms-slider-secondary-navigation .swiper-button-next',
+		},
+		breakpoints: {
+			768: {
+				centeredSlides: true,
+				slidesPerView: 4,
+				slideToClickedSlide: false,
+			},
+			992: {
+				centeredSlides: false,
+				initialSlide: 1,
+				slidesPerView: 1.5,
+				slideToClickedSlide: true,
+			},
+		},
+	});
+
+	const stylingRoomsMainSwiper = new Swiper('.s-rooms-slider-main', {
+		loop: true,
+		allowTouchMove: false,
+		spaceBetween: 20,
+		speed: 600,
+	});
+
+	stylingRoomsSecondarySwiper.on('slideNextTransitionStart', (swiper) => {
+		stylingRoomsMainSwiper.slideNext();
+	});
+
+	stylingRoomsSecondarySwiper.on('slidePrevTransitionStart', (swiper) => {
+		stylingRoomsMainSwiper.slidePrev();
+	});
+
+	/**
+	 * Services slider
+	 */
+	const stylingPackagesSwiper = new Swiper('.s-packages-slider', {
+		slidesPerView: 1,
+		spaceBetween: 20,
+		speed: 500,
+		navigation: {
+			prevEl: '.s-packages-slider-navigation .swiper-button-prev',
+			nextEl: '.s-packages-slider-navigation .swiper-button-next',
+		},
+		breakpoints: {
+			576: {
+				slidesPerView: 2,
+			},
+			992: {
+				slidesPerView: 3,
+			}
+		},
+	});
 
 });
